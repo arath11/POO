84b4954f646493d5f1313a39de2eab0c6dc7a45c
arath11 <35709739+arath11@users.noreply.github.com>
2019-10-04 20:52:18 -0500

    Graphic interface for VentanaPalomino

---
+++ Ventana/Ventana.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame{
+	public Ventana(){
+		super("Diana");
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(arg0);
+	}
+}
